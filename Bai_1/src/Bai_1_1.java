8ddcd7bc95a013a30f5331f886d708cd7c15370f
keenlight96 <nguyenkimlong29196@gmail.com>
2023-05-04 14:18:46 +0700

    id=9088

--- Bai_1/src/Bai_1_1.java
+++ Bai_1/src/Bai_1_1.java
@@ -1,5 +1,8 @@
+import java.util.Date;
+
 public class Bai_1_1 {
     public static void main(String[] args) {
-        
+        Date now = new Date();
+        System.out.println("Now is :" + now);
     }
 }
