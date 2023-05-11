0bc680590e9e005e714c0d57c4d8567a8189e55e
keenlight96 <nguyenkimlong29196@gmail.com>
2023-05-11 14:36:39 +0700

    new

---
+++ Day6/src/Task4_id9174/Main.java
@@ -0,0 +1,11 @@
+package Task4_id9174;
+
+public class Main {
+    public static void main(String[] args) {
+        MovablePoint obj = new MovablePoint(2,2,2,2);
+        obj.move();
+        obj.move();
+        System.out.println(obj);
+        System.out.println(obj.move());
+    }
+}
