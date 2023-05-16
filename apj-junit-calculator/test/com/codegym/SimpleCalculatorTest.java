53bf3a47dff0e564a4dc23da9cffe14afed2f760
keenlight96 <nguyenkimlong29196@gmail.com>
2023-05-16 09:06:12 +0700

    new

--- apj-junit-calculator/test/com/codegym/SimpleCalculatorTest.java
+++ apj-junit-calculator/test/com/codegym/SimpleCalculatorTest.java
@@ -1,10 +1,9 @@
 package com.codegym;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimpleCalculatorTest {
 
