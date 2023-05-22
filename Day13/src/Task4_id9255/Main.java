cc142c7f72b73fe64f4337a9673f60f8b6c18f79
keenlight96 <nguyenkimlong29196@gmail.com>
2023-05-22 15:28:16 +0700

    1

---
+++ Day13/src/Task4_id9255/Main.java
@@ -0,0 +1,8 @@
+package Task4_id9255;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3,5,10,35,205,386};
+        System.out.println(BinarySearch.search(arr, 5));
+    }
+}
