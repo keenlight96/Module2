593beb502fa15bce4ac9d9de01376f097a3c0020
keenlight96 <nguyenkimlong29196@gmail.com>
2023-05-23 14:26:21 +0700

    1

---
+++ Day14/src/Task5_id9262/Main.java
@@ -0,0 +1,44 @@
+package Task5_id9262;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input array length: ");
+        int n = Integer.parseInt(sc.nextLine());
+        System.out.println("Input array: ");
+        int[] ar = new int[n];
+        for (int i = 0; i < n; i++) {
+            ar[i] = sc.nextInt();
+        }
+
+        insertionSort(ar);
+        show(ar);
+    }
+
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j;
+            for (j = i - 1; j >= 0; j--) {
+                if (temp < array[j])
+                    array[j + 1] = array[j];
+                else
+                    break;
+            }
+            array[j + 1] = temp;
+        }
+    }
+
+    public static void show(int[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            if (i != array.length - 1)
+                System.out.print(array[i] + ", ");
+            else
+                System.out.print(array[i]);
+        }
+        System.out.print("]");
+    }
+}
