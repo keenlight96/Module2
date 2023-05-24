184f824ba6d3950b867b70c0214721902972d99a
keenlight96 <nguyenkimlong29196@gmail.com>
2023-05-24 16:41:18 +0700

    1

--- Day15/src/Task2_id9276/Main.java
+++ Day15/src/Task2_id9276/Main.java
@@ -1,15 +1,31 @@
 package Task2_id9276;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int[] ar = createRandom();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input index: ");
+        int index = Integer.parseInt(scanner.nextLine());
+        try {
+            System.out.println(ar[index]);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index out of bound");
+        }
 
+        System.out.println("End.");
     }
 
-    public int[] createRandom() {
+    public static int[] createRandom() {
         Random random = new Random();
         int[] ar = new int[100];
-        
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = random.nextInt(100);
+            System.out.print(ar[i] + " ");
+        }
+        System.out.println();
+        return ar;
     }
 }
